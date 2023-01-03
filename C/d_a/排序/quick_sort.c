cdf62e9820cb269e5d54d86fee46fea7929f3e65
HzfinChina <888iagiag888@gmail.com>
2023-01-03 18:57:22 +0800

    delete test code

--- C/d_a/排序/quick_sort.c
+++ C/d_a/排序/quick_sort.c
@@ -22,23 +22,3 @@ void quick_sort(int *array, int left, int right){
     quick_sort(array, left, i);
     quick_sort(array, i+1, right);
 }
-
-#include <stdio.h>
-#include <time.h>
-#define LEN 100000
-int main()
-{
-    int array[LEN] = {};
-    FILE *fp = fopen("/tmp/s", "r");
-    for (int i = 0; i < LEN; i++){
-        fscanf(fp, "%d, ", &array[i]);
-    }
-    clock_t start, end;
-    start = clock();
-    quick_sort(array, 0, LEN -1);
-    for (int i = 0; i < LEN; i++)
-        printf("%d ", array[i]);
-    end = clock();
-    double duration = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("%f",duration);
-}
